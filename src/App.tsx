cf3ff359be2ff554858171d2254f535e3414f840
TBRWEB - Desenvolvimento de Sites <macsueldias@hotmail.com>
2023-05-15 11:32:24 -0300

    refactor: app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import { useEffect, useState } from 'react';
 
-import { Button } from './components/Button';
-import { MovieCard } from './components/MovieCard';
-
-// import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
 
 import { api } from './services/api';
 
@@ -12,6 +7,10 @@ import './styles/global.scss';
 
 import './styles/sidebar.scss';
 import './styles/content.scss';
+
+import { Content } from './components/Content';
+import { SideBar } from './components/SideBar';
+import { Header } from './components/Header';
 
 interface GenreResponseProps {
   id: number;
@@ -58,37 +57,19 @@ export function App() {
     setSelectedGenreId(id);
   }
 
+  const dataSideBar = {genres, selectedGenreId, handleClickButton}
+
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
       <nav className="sidebar">
         <span>Watch<p>Me</p></span>
-
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
+        <SideBar data={dataSideBar}/>
       </nav>
 
       <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
+        <Header title={selectedGenre.title} />
 
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
+        <Content movies={movies} />
       </div>
     </div>
   )
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface Genres {
+    id: number
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family'
+    title: string
+}
+
+interface SideBarProps {
+  genres: Genres[]
+  selectedGenreId: number
+  handleClickButton: (id: number) => void
+}
+
+interface DataProps {
+  data: SideBarProps
+}
+
+export function SideBar({data} : DataProps) {
+
+    return (
+    <div className="buttons-container">
+      {data.genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => data.handleClickButton(genre.id)}
+          selected={data.selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+
+  )
 }
